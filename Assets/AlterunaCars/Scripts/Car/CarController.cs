206589162c248f13c57a063a5f9aac4ff28aa729
Muhammad Fidaul Haq <vivalafida123@gmail.com>
2024-11-07 08:37:37 +0700

    jasfkdjsf

--- Assets/AlterunaCars/Scripts/Car/CarController.cs
+++ Assets/AlterunaCars/Scripts/Car/CarController.cs
@@ -256,7 +256,7 @@ namespace AlterunaCars
         private void SetPosition()
         {
             // transform.position = new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30));
-            transform.localPosition = new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30));
+            transform.position = new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30));
             _rb.constraints = RigidbodyConstraints.None;
         }
     }
